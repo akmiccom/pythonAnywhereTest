8e47ae44fd5d31e712e2e41ec5ace660f87ef816
akmiccom <akmiccom@gmail.com>
2023-10-27 16:42:50 +0900

    today's final commit

--- EnglishApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EnglishappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'EnglishApp'
--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include('hello.urls')),
-    path('english/', include('EnglishApp.urls')),
+    # path('english/', include('EnglishApp.urls')),
 ]
