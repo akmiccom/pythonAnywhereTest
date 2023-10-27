951e7f2fc490b1d99109697e5e46f7f43337f48d
akmiccom <akmiccom@gmail.com>
2023-10-27 11:37:47 +0900

    文字修正

--- hello/views.py
+++ hello/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 
 html = '''
 <h1>Hello World.</h1>
-<h2>アップデーとのテスト ➡ ファイルを直接修正</h2>
-<h2>アップデーとのテスト ➡ githubから修正</h2>
-<h2>アップデーとのテスト ➡ 構築を手動でやってみる</h2>
-<h2>アップデーとのテスト ➡ git pull</h2>
+<h2>アップデートのテスト ➡ ファイルを直接修正</h2>
+<h2>アップデートのテスト ➡ githubから修正</h2>
+<h2>アップデートのテスト ➡ 構築を手動でやってみる</h2>
+<h2>アップデートのテスト ➡ git pull</h2>
 '''
 
 def index(request):
