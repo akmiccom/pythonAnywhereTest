5dbda81396268066e41bfef00617043729041276
akmiccom <akmiccom@gmail.com>
2023-10-27 10:07:48 +0900

    2nd test commit

--- hello/views.py
+++ hello/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 
 html = '''
 <h1>Hello World.</h1>
-<h2>アップデーとのテストをしています</h2>
+<h2>アップデーとのテスト ➡ ファイルを直接修正</h2>
+<h2>アップデーとのテスト ➡ githubから修正</h2>
 '''
 
 def index(request):
